df71e46b189a86d56111fd06356dff4d10fb9edb
Chandramouli Haldar <148478906+Chandramouli001@users.noreply.github.com>
2024-04-20 21:06:21 +0530

    Update app.js

--- rock paper scissors/app.js
+++ rock paper scissors/app.js
@@ -1,75 +1,4 @@
 
-// var uin, ip, rp, rs;
-// var hcount=0,rcount=0,ti=0;
-
-
-// var rock = document.getElementById("ro");
-// var paper = document.getElementById("pa");
-// var scissors = document.getElementById("sc")
-// var score = document.querySelector("div")
-// var ysc = document.getElementById("ysc")
-// var rsc = document.getElementById("rsc")
-// var tie = document.getElementById("tie")
-// var ytext = document.getElementById("ytext")
-// var wtext =document.getElementById("wtext")
-// var rtext =document.getElementById("rtext")
-
-// rock.addEventListener("click", rfunc =>{
-//     uin = 1;
-//     logic()
-// })
-// paper.addEventListener("click", rfunc =>{
-//     uin = 2;
-//     logic()
-// })
-// scissors.addEventListener("click", rfunc =>{
-//     uin = 3;
-//     logic()
-// })
-
-
-// function logic (){
-// if (uin == 1) {
-//     ip = "rock";
-// } else if (uin == 2) {
-//     ip = "paper";
-// } else if (uin == 3) {
-//     ip = "scissors";
-// }
-
-// var rb = Math.floor(Math.random() * 3) + 1;
-
-// if (rb == 1) {
-//     rp = "rock";
-// } else if (rb == 2) {
-//     rp = "paper";
-// } else if (rb == 3) {
-//     rp = "scissors";
-// }
-
-// if ((rb == 1 && uin == 1) || (rb == 2 && uin == 2) || (rb == 3 && uin == 3)) {
-//     rs = ("It's a tie!"); ti++;
-// } else if ((rb == 1 && uin == 3) || (rb == 2 && uin == 1) || (rb == 3 && uin == 2)) {
-//     rs = ("The robot wins!");
-//     rcount++
-// } else if ((uin == 1 && rb == 3) || (uin == 2 && rb == 1) || (uin == 3 && rb == 2)) {
-//     rs = ("You win!");
-//     hcount++
-// }
-
-// console.log(`You chose ${ip} \nthe robot chose ${rp} \n${rs}`);
-// console.log(`total score : \nrobo:${rcount}\ntie:${ti}\nyou:${hcount}`)
-// ysc.innerText = hcount;
-// rsc.innerText = rcount;
-// tie.innerText = ti;
-// wtext.innerText = rs;
-// ytext.innerText = ip
-// rtext.innerText = rp
-
-// }
-
-
-
 
 var uin, ip, rp, rs;
 var hcount = 0, rcount = 0, ti = 0;
@@ -78,9 +7,9 @@ var hcount = 0, rcount = 0, ti = 0;
 var rock = document.getElementById("ro");
 var paper = document.getElementById("pa");
 var scissors = document.getElementById("sc")
-var rrock = document.getElementById("rro");
-var rpaper = document.getElementById("rpa");
-var rscissors = document.getElementById("rscc")
+var rro = document.getElementById("rro");
+var rpa = document.getElementById("rpa");
+var rscc = document.getElementById("rscc")
 var score = document.querySelector("div")
 var ysc = document.getElementById("ysc")
 var rsc = document.getElementById("rsc")
@@ -130,13 +59,13 @@ function logic() {
     } else if (rb == 2) {
         rp = "paper";
         rro.style.backgroundColor = `#D678FA`;
-        rscc.style.backgroundColor = color;
-        rpa.style.backgroundColor = `#D678FA`;
+        rpa.style.backgroundColor = color;
+        rscc.style.backgroundColor = `#D678FA`;
     } else if (rb == 3) {
         rp = "scissors";
         rro.style.backgroundColor = `#D678FA`;
-        rscc.style.backgroundColor = `#D678FA`;
-        rpa.style.backgroundColor = color;
+        rpa.style.backgroundColor = `#D678FA`;
+        rscc.style.backgroundColor = color;
     }
 
     if ((rb == 1 && uin == 1) || (rb == 2 && uin == 2) || (rb == 3 && uin == 3)) {
@@ -155,7 +84,5 @@ function logic() {
     rsc.innerText = rcount;
     tie.innerText = ti;
     wtext.innerText = rs;
-    ytext.innerText = ip
-    rtext.innerText = rp
 
 }
